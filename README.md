48fe601341d36b9dfabafaad97d16623377503da
anapaulacd <anapaulacoqueirodogado@gmail.com>
2024-03-13 11:05:15 -0300

    escrevi o readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # php
+# códigos matematicos
+![foto](img)
+ 
+## Descrição
+Esse é um site para ajudaros usuários com contas matemáticas como atividades matemáticas como.
+![git]()
+
+## Funcionalidades
+* Verificar se o número é IMPAR
+* Verificar se o número é PAR
+* Verificar se o número é redondo
+* Verificar se o número é negativo 
+* verificar se o número é positivo 
+* Verificar se o número é neutro
+
+## 💻 tecnologias utilizadas
+* VsCode
+* HTML
+* php
+* Google
+* Git
+* Github
+* Captura de tela
+ 
+## 📄 Aprendizados
+Nesse projeto foi para a introdução do meu aprendizado em php, o php usado juntamente com  o HTML para entender um pouco como é a linguagem php. Os conseitos aprendidos foram de estrutura de condição no php utlizando if e else.
+ 
+# autores
+[Ana Paula](https://github.com/anapaulacd)
+orientador [Leonardo](https://github.com/LeonardoRochaMarista)
+ 
--- atv2.php
+++ atv2.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1> Par ou Impar</h1>
     <form method="post">
-     <label for="number">Coloque um número:</label><br>
-    <input type="numero" id="number" name="number"crequired>
-    <button type="subimit">verificar</button>
+        <label for="number">Coloque um número:</label><br>
+        <input type="number" id="number" name="number" crequired>
+        <button type="subimit">verificar</button>
     </form>
-    <?php 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $number = $_POST["number"];
-        if ($number % 2 == 0) {
-            echo "<p>o numero é PAR.</p>";
-        } else {
-            echo "<p>o numero e IMPAR.<?P>";
-
-        }
-    } 
+    <?php
    
-        if ($number > 0) {
-            echo "<p>o numero e positivo </p>";
-        } elseif ($number< 0)   {
-            echo "<p>o numero e negativo <?P>";
-        } else {
-            echo "<p>o numero inserido é neutro (zero).</p>";
-        }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $number = $_POST ['number'];
 
-        if ($number % 10 == 0) {
-            echo "<p>o numero é redondo </p>";
-        } else {
-            echo "<p>o numero não é redomdo <?P>";
-        } 
+    if ($number % 2 == 0){
+        echo "<p>o numero é PAR.</p>";
+        }else{
+        }echo "<p>o numero e IMPAR.</p>";
+    }
+
+if ($number > 0){
+    echo "<p>o numero e positivo </p>";
+ } elseif ($number < 0){
+    echo "<p>o numero e negativo <?P>";
+ }else{
+ echo "<p>o numero inserido é neutro (zero).</p>";
+ }
+if ($number % 10 == 0){
+  echo "<p>o numero é redondo </p>";
+}else{
+  echo "<p>o numero não é redomdo <?P>";
+}
     ?>
-</form>
+    
 </body>
+
 </html>
